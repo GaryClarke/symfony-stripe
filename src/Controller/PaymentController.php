cc60949415b632f963cbe919a68cbda24fa1cdfc
Gary Clarke <garyclarke1974@gmail.com>
2021-08-02 21:34:28 +0100

    Commit lesson files

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class PaymentController extends AbstractController
+{
+
+    #[Route('/payment', name: 'payment')]
+    public function index(): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+
+
+    #[Route('/checkout', name: 'checkout')]
+    public function checkout($stripeSK): Response
+    {
+        Stripe::setApiKey($stripeSK);
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items'           => [
+                [
+                    'price_data' => [
+                        'currency'     => 'usd',
+                        'product_data' => [
+                            'name' => 'T-shirt',
+                        ],
+                        'unit_amount'  => 2000,
+                    ],
+                    'quantity'   => 1,
+                ]
+            ],
+            'mode'                 => 'payment',
+            'success_url'          => $this->generateUrl('success_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url'           => $this->generateUrl('cancel_url', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        return $this->redirect($session->url, 303);
+    }
+
+
+    #[Route('/success-url', name: 'success_url')]
+    public function successUrl(): Response
+    {
+        return $this->render('payment/success.html.twig', []);
+    }
+
+
+    #[Route('/cancel-url', name: 'cancel_url')]
+    public function cancelUrl(): Response
+    {
+        return $this->render('payment/cancel.html.twig', []);
+    }
+}
+
+
+
+
+
+
+
+
+
+
